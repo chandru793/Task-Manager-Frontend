d549ffbb68c1fdfd8d32686b8516ff8e7b015840
chandru793 <67958864+chandru793@users.noreply.github.com>
2023-02-09 09:22:53 +0530

    useContext Updated

--- src/context/TaskContext.js
+++ src/context/TaskContext.js
@@ -1,5 +1,3 @@
-import {
-    createContext
-} from "react";
-    
-export const  TaskContext = createContext({})
+import { createContext } from "react";
+
+export const TaskContext = createContext({});
