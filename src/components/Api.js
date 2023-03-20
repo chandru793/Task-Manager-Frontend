38f479b5e0f5dd0018f8e18d1e4e8edfd1223d5b
chandru793 <67958864+chandru793@users.noreply.github.com>
2023-03-20 09:29:34 +0530

    deploy

--- src/components/Api.js
+++ src/components/Api.js
@@ -2,21 +2,21 @@ import axios from "axios";
 
 //api to create new task
 export const createTask = async (task,description) => {
-  await axios.post("http://localhost:8080/api/v1/tasks", { name: task,description:description });
+  await axios.post(`${process.env.REACT_APP_HOST_URL}/api/v1/tasks`, { name: task,description:description });
 };
 
 //api to get all tasks
 export const getAllTasks = async () =>
-  await axios.get("http://localhost:8080/api/v1/tasks");
+  await axios.get(`${process.env.REACT_APP_HOST_URL}/api/v1/tasks`);
 
 //api to update a the completed
 export const updateCompleted = async (id,completed,dolater) =>
-  await axios.patch(`http://localhost:8080/api/v1/tasks/${id}`, {completed:completed,dolater:false});
+  await axios.patch(`${process.env.REACT_APP_HOST_URL}/api/v1/tasks/${id}`, {completed:completed,dolater:false});
 
 //api to update a task name
 export const updateTask = async (id,name,description,dolater) =>
-  await axios.patch(`http://localhost:8080/api/v1/tasks/${id}`, { name:name,dolater:dolater });
+  await axios.patch(`${process.env.REACT_APP_HOST_URL}/api/v1/tasks/${id}`, { name:name,dolater:dolater });
 
 //api to delete a task
 export const deleteTask = async (id) =>
-  await axios.delete(`http://localhost:8080/api/v1/tasks/${id}`);
+  await axios.delete(`${process.env.REACT_APP_HOST_URL}/api/v1/tasks/${id}`);
